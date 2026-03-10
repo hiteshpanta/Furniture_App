5fc1a7f932f63c009d2ebf640d6254c6e917ed93
__Hitesh__ <--hiteshpant50@gmail.com>
2026-03-10 23:06:11 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import blogRoutes from './routes/blogRoutes.js';
+import mongoose from 'mongoose';
+import fileUpload from 'express-fileupload';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import path from 'path'
+
+const app = express();
+const port = 5000;
+
+dotenv.config();
+
+
+
+mongoose.connect(process.env.DB_URL).then((val) => {
+  app.listen(port, () => {
+    console.log('connected and server is running ');
+  });
+}).catch((err) => {
+  console.log(err);
+});
+
+
+
+
+app.use(cors())
+app.use(express.json());
+app.use(fileUpload({
+  limits: { fileSize: 5 * 1024 * 1024 },
+}));
+
+// app.use('/uploads' ,express.static(path.join(process.cwd(),'uploads')));
+
+app.use(express.static('uploads'));
+
+
+
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    status: 'success',
+    data: 'hello jee welcome to Server'
+  });
+});
+
+
+
+
+app.use(userRoutes);
+app.use(productRoutes);
+app.use(orderRoutes);
+app.use(blogRoutes);
+
+
+
+
+
+
+
